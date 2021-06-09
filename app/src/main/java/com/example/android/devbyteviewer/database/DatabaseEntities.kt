202938cff38094710a3012d9b5802c35df677d50
HUANG-CHI-RUEI <pei4b066@gmail.com>
2021-06-09 20:45:08 +0800

    Build and use a repository

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -28,18 +28,16 @@ data class DatabaseVideo constructor(
     val updated: String,
     val title: String,
     val description: String,
-    val thumbnail: String
-)
+    val thumbnail: String)
 
 fun List<DatabaseVideo>.asDomainModel(): List<Video> {
-    return map{
+    return map {
         Video(
             url = it.url,
             title = it.title,
             description = it.description,
             updated = it.updated,
-            thumbnail = it.thumbnail
-        )
+            thumbnail = it.thumbnail)
     }
 }
 
